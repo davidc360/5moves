7ce044bd3886da369ae11c5ab45d3a54d7758ad0
David Cao <davidcao@Davids-MacBook-Pro.local>
2021-03-16 22:13:16 -0400

    can detect winner

--- pages/game/[id].js
+++ pages/game/[id].js
@@ -10,22 +10,68 @@ export default function Game({ data }) {
     const gridValuesRef = useRef([...Array(9)])
     const [movesPicked, setMovesPicked] = useState(false)
 
-    const MARKS = {
-        O: <CircleMark />,
-        X: <XMark />
-    }
-
     const grids = [...Array(9)].map((_, i) => (
         <div key={i} id={i} onClick={addMove}>
             {gridValues[i] ? MARKS[gridValues[i]] : ''}
         </div>
     ))
 
-    function addMove(e) {
-        if(moves.length<9)
-            setMoves([...moves, e.target.id])
+    function findWinner(grid=gridValues) {
+        // transform grid into 3 rows
+        const matrix = [gridValues.slice(0,3), gridValues.slice(3,6), gridValues.slice(6)]
 
-        // setMoves('678345012'.split(''))
+        // check for connecting horizontal and vertical lines
+        let winner
+        matrix.forEach(
+            (row, i) => {
+                // check if the whole row is same, which means a line
+                if (containsSame(row)) {
+                    winner = row[0]
+                }
+                
+                // check if [i] column is the same
+                const column = matrix.map(row_ => row_[i])
+                if (containsSame(column)) {
+                    winner = column[0] 
+                }
+            }
+        )
+
+        // check for diagonal lines
+        // first there has to be a center mark
+        const centerMark = matrix[1][1]
+        if (centerMark) {
+            // check if top left and bottom right is same mark
+            if (matrix[0][0] === centerMark && matrix[2][2]) {
+                winner = centerMark
+            } else if (matrix[0][2] === centerMark && matrix[2][0]) {
+                // else check if top right is same as bottom left
+                winner = centerMark
+            }
+        }
+
+        return winner
+    }
+    // find winner
+    const [winner, setWinner] = useState()
+    useEffect(() => {
+        // use a temp winner variable to avoid waiting for
+        // "setWinner" to take effect in changing the state
+        // and clear timeouts immediately
+        let winner_ = findWinner()
+        if (winner_) timeouts.current.forEach(timeout => clearTimeout(timeout))
+
+        setWinner(winner_)
+    }, [gridValues])
+
+    function containsSame(arr) {
+        if (arr[0] === undefined || arr[0] === null) return false
+        return arr.every(v => v === arr[0]) 
+    }
+
+    function addMove(e) {
+        // if(moves.length<9) setMoves([...moves, e.target.id])
+        setMoves('678345012'.split(''))
     }
     useEffect(() => {
         if (moves.length === 9) setMovesPicked(true)
@@ -35,30 +81,31 @@ export default function Game({ data }) {
         if (gridValuesRef.current[move] === mark) {
             gridValuesRef.current[move] = null
             setGridValues([...gridValuesRef.current])
-            gridValuesRef.current[move] = mark
-            setGridValues([...gridValuesRef.current]) 
-        } else {
-            gridValuesRef.current[move] = mark
-            setGridValues([...gridValuesRef.current])
         }
+        gridValuesRef.current[move] = mark
+        setGridValues([...gridValuesRef.current])
     }
     
-    const speed = 1000
+    const timeouts = useRef([])
+    // how long each move takes
+    const speed = 700
     useEffect(() => {
+        let int
         if (movesPicked) {
             for (let i = 0; i < 9; i++) {
-                setTimeout(() => {
+                timeouts.current.push(setTimeout(() => {
                     // set opponent's move
                     addMoveToGrid(data.moves[i], 'X')
 
                     // set self move
-                    setTimeout(() => {
+                    timeouts.current.push(setTimeout(() => {
                             addMoveToGrid(moves[i], 'O')
-                    }, speed)
-
-                }, i*speed*2)
+                    }, speed))
+                }, i*speed*2))
             }
         }
+
+        return () => clearInterval(int)
     }, [movesPicked])
 
     const GameComponent = (
@@ -101,8 +148,13 @@ const XMark = () => (
 
 const CircleMark = () => (
     <svg className={styles.mark} viewBox="0 0 52 52">
-        <circle className={styles.circle} cx="50%" cy="50%" r="10" stroke-dasharray="360">
-            <animate attributeName="stroke-dashoffset" values="360;0" dur="3s" repeatCount="forward"></animate>
+        <circle className={styles.circle} cx="50%" cy="50%" r="10" strokeDasharray="360">
+            <animate attributeName="stroke-dashoffset" values="360;0" dur="2.5s" repeatCount="forward"></animate>
         </circle>
     </svg>
 )
+
+const MARKS = {
+    O: <CircleMark />,
+    X: <XMark />
+}
