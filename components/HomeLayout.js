d0f3e93b743997e68688c63d300ff5b173e12ff1
David Cao <davidcao@Davids-MBP.fios-router.home>
2021-03-18 16:35:06 -0400

    refractored folder strcture

---
+++ components/HomeLayout.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import styles from './Home.module.sass'
+
+export default function HomeLayout ({ content }) {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Tic Tac Toe</title>
+            </Head>
+
+            <main className={styles.main}>
+                {content}
+            </main>
+
+            <footer className={styles.footer}>
+                <a
+                href="https://github.com/davidc360/TicTacToe"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                {' '}
+                <img src="/GitHub-Mark-120px-plus.png" alt="Github Logo" className={styles.logo} />
+                </a>
+            </footer>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,8 @@
-import Head from 'next/head'
-import styles from './Home.module.sass'
-
 import Game from './game/[id].js'
+import HomeLayout from '../components/HomeLayout.js'
 
 export default function Home() {
     return (
-        <HomeLayout content={<Game />}/>
-    )
-}
-
-
-export function HomeLayout({ content }) {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Tic Tac Toe</title>
-            </Head>
-
-            <main className={styles.main}>
-                {content}
-            </main>
-
-            <footer className={styles.footer}>
-                <a
-                href="https://github.com/davidc360/TicTacToe"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                {' '}
-                <img src="/GitHub-Mark-120px-plus.png" alt="Github Logo" className={styles.logo} />
-                </a>
-            </footer>
-        </div>
+        <Game />
     )
 }
