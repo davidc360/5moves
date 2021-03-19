087097fef29836ece8f09e1d16bb4e8843c221fa
David Cao <davidcao@Davids-MBP.fios-router.home>
2021-03-18 22:19:41 -0400

    fixed env var

--- components/Game.js
+++ components/Game.js
@@ -159,7 +159,7 @@ export default function Game({ data }) {
                 .then(res => {
                     // stop the loading dots
                     clearInterval(loadingDotsInt)
-                    const link = process.env.DEPLOYMENT_LINK + 'game/' + res.data
+                    const link = process.env.PUBLIC_NEXT_DEPLOYMENT_LINK + 'game/' + res.data
                     nameInputRef.current.value = link
                     nameInputRef.current.select()
                     document.execCommand('copy')
